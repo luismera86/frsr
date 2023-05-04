2712a0afa94a6f117f934e410afdf94a6ebad64c
Luis Mera <luismera86@gmail.com>
2023-05-04 00:12:12 -0300

    Primeras prácticas con Java

---
+++ Java2023/clase 2/Clase2/src/HolaMundo.java
@@ -0,0 +1,19 @@
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde java");
+
+        int miVariable = 10;
+
+        System.out.println(miVariable);
+        // Re asignamos la variable
+        miVariable = 20;
+        System.out.println(miVariable);
+        // Tipo String
+        String miVariableCadena = "Bienvenidos";
+        System.out.println(miVariableCadena);
+        // re asignamos la variable de la cadena
+        miVariableCadena = "Seguimos creciendo en programación";
+        System.out.println(miVariableCadena);
+    }
+}
