c14f44411465c29ed3c8ede8155bfd00db2379c6
Luis Mera <luismera86@gmail.com>
2023-05-18 02:42:07 -0300

    ampliación de clase 3

--- Java2023/Clase3/src/Main.java
+++ Java2023/Clase3/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
     public static void main(String[] args) {
         // Especifico qeu tipo de variable va a ser
         int miVaribale = 10;
-
+        System.out.println("miVaribale = " + miVaribale);
         // El uso de var hace que las variables sean dinámicas java infiere el tipo de variable
         var miVariableEntera = 2;
-
+        System.out.println("miVariableEntera = " + miVariableEntera);
     }
 }
