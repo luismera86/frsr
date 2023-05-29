14c7494b13a7c74f9ac96dff900f6e5535ddc4bb
Luis Mera <luismera86@gmail.com>
2023-05-28 21:05:34 -0300

    Modulos de clases cambiados

--- Java2023/Clase - 2/src/HolaMundo.java
+++ Java2023/Clase - 2/src/HolaMundo.java
@@ -1,19 +1,15 @@
 public class HolaMundo {
+    public HolaMundo() {
+    }
 
     public static void main(String[] args) {
         System.out.println("Hola mundo desde java");
-
         int miVariable = 10;
-        
-
         System.out.println(miVariable);
-        // Re asignamos la variable
         miVariable = 20;
         System.out.println(miVariable);
-        // Tipo String
         String miVariableCadena = "Bienvenidos";
         System.out.println(miVariableCadena);
-        // re asignamos la variable de la cadena
         miVariableCadena = "Seguimos creciendo en programación";
         System.out.println(miVariableCadena);
     }
--- Java2023/Clase - 3/src/Main.java
+++ Java2023/Clase - 3/src/Main.java
@@ -1,11 +1,13 @@
-
 public class Main {
+    public Main() {
+    }
+
     public static void main(String[] args) {
-        // Especifico qeu tipo de variable va a ser
         int miVaribale = 10;
         System.out.println("miVaribale = " + miVaribale);
-        // El uso de var hace que las variables sean dinámicas java infiere el tipo de variable
-        var miVariableEntera = 2;
+        int miVariableEntera = 2;
         System.out.println("miVariableEntera = " + miVariableEntera);
+        String nombre = "Luis";
+        System.out.println("Nombre \n " + nombre);
     }
 }
