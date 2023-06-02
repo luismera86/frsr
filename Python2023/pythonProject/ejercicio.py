56921501f087360b445b8d67f7b9da67632b2790
Luis Mera <luismera86@gmail.com>
2023-06-02 02:20:53 -0300

    Clase 4

---
+++ Python2023/pythonProject/ejercicio.py
@@ -0,0 +1,8 @@
+# Calcular el área de un rectángulo
+
+alto = int(input("Proporciona el alto del rectángulo: "))
+ancho = int(input("Proporciona el ancho del rectángulo: "))
+area = alto * ancho
+perimetro = (alto + ancho) * 2
+print(f"El area del Rectángulo es : {area}")
+print(f"El perímetro del Rectángulo es: {perimetro}")
---
+++ Python2023/pythonProject/main.py
@@ -0,0 +1,32 @@
+
+operandoA = 8
+operandoB = 5
+suma = operandoA + operandoB
+print(f"El resultado de la suma es: {suma}")  # Esta es la forma de hacer una interporlación
+
+resta = operandoA - operandoB
+print(f"El resultado de la resta es: {resta}")
+
+multiplicacion = operandoA * operandoB
+print(f"El resultado de la multipliación es: {multiplicacion}")
+
+division = operandoA / operandoB
+print(f"El resultado de la división es: {division}")
+division = operandoA // operandoB
+print(f"El resultado de la división (int) es: {division}")
+
+modulo = operandoA % operandoB
+print(f"El resultado del modulo es: {modulo}")
+
+exponente = operandoA ** operandoB
+print(f"El resultado del exponente es: {exponente}")
+
+# Operadores de asignación
+
+miVariable = 10  # le asignamos el valor de 10
+miVariable = miVariable + 1  # Hacemos una reasiganción aumentando el valor en 1
+miVariable += 1  # lo mismo que lo anterior aumentamos en 1 el valor
+miVariable -= 1
+miVariable *= 2
+miVariable /= 2
+miVariable %= 2
