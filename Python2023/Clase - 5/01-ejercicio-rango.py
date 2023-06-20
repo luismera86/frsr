9c96e27ba089125405a92a8e452a81f113123875
Luis Mera <luismera86@gmail.com>
2023-06-20 17:21:56 -0300

    Carpeta java elimiada

---
+++ Python2023/Clase - 5/01-ejercicio-rango.py
@@ -0,0 +1,8 @@
+#Ejercicio: Ranggo entre 20 y 30
+
+edad = int(input("Ingrese su edad: "))
+if edad >= 20 and edad <= 30:
+    print("Su edad esta en el rango de 20 a 30 años")
+else:
+    print("Su edad no esta en el rango de 20 a 30 años")
+  
