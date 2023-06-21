524241411732d2634ac54352d7f53a43e640e1b0
Luis Mera <luismera86@gmail.com>
2023-06-20 23:19:26 -0300

    Ejercicio 1

--- Python2023/Clase - 5/01-ejercicio-rango.py
+++ Python2023/Clase - 5/01-ejercicio-rango.py
@@ -1,14 +1,17 @@
-#Ejercicio: Rango entre 20 y 30
+# Ejercicio: Rango entre 20 y 30
 
 edad = int(input("Ingrese su edad: "))
-veintes = edad >= 20 and edad < 30
+veinte = edad >= 20 and edad < 30
 print(veintes)
-treintas = edad >= 30 and edad < 40
+treinta = edad >= 30 and edad < 40
 print(treintas)
 
-if veintes or treintas:
-    print("Estás en tus 20 o 30")
+# Sintaxis simplificada del operador lógico AND
+if (20 <= edad < 30) or (30 <= edad < 40):
+    print("Estas dentro del rango de los 20 o 30 años")
 else:
-    print("No estás en tus 20 o 30")
+    print("No estas dentro del rango de los 20 o 30 años")
+
+
 
 
