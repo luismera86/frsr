2c6828941bee844d696a5c30a6f9fe2aef9a3d01
Luis Mera <luismera86@gmail.com>
2023-06-20 23:56:23 -0300

    Clase 6 if else

---
+++ Python2023/Clase - 6/main.py
@@ -0,0 +1,13 @@
+# Sentecias if - else
+
+condicion = 0
+# condicion = "" quiere decir que la variable esta vacia y es igual a False
+# condicion = "algo" quiere decir que la variable tiene algo y es igual a True
+# condicion = 0 quiere decir que la variable tiene un valor y es igual a False
+# condicion = 1 quiere decir que la variable tiene un valor y es igual a True
+
+if condicion:
+    print("La condicion es verdadera")
+else:
+    print("La condicion es falsa")
+
