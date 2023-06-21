883c38e447059619ca57f0750e352901d7f90496
Luis Mera <luismera86@gmail.com>
2023-06-21 00:16:53 -0300

    Clase 6 if else

--- Python2023/Clase - 6/main.py
+++ Python2023/Clase - 6/main.py
@@ -11,3 +11,14 @@ if condicion:
 else:
     print("La condicion es falsa")
 
+# Conversión de números a texto
+num = int(input("Escribe un número del 1 al 3: "))
+if num == 1:
+    numTexto = "Número uno"
+elif num == 2:
+    numTexto = "Número dos"
+elif num == 3:
+    numTexto = "Número tres"
+else:
+    numTexto = "Has ingresado un valor fuera de rango"
+    print(f"El numero ingresado es : {num} - {numTexto}")
