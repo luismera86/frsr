c0615040b06f2d6f18d21e73a223f162c602da95
Luis Mera <luismera86@gmail.com>
2023-07-05 00:01:08 -0300

    Ejercicio 3 clase 7 python

---
+++ Clase - 7/Ejercicios/Ejercicio3.py
@@ -0,0 +1,13 @@
+
+mes_anio = int(input("Ingrese el mes del año entre 1 y 12: "))
+
+if mes_anio <= 3:
+    print("Es Verano")
+elif mes_anio > 3 and mes_anio <= 6:
+    print("Es Otonño")
+elif mes_anio > 6 and mes_anio <= 9:
+    print("Es Invierno")
+elif mes_anio > 9 and mes_anio <= 12:
+    print("Es primavera")
+elif mes_anio > 1 or mes_anio < 12:
+    print("El numero ingresado debe ser entre 1 y 12")gi
