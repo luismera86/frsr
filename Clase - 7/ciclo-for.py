8032feae2539e9dd8f47d93a8ba5616bfa34c0d3
Luis Mera <luismera86@gmail.com>
2023-07-04 23:57:12 -0300

    Cilco for

--- Clase - 7/ciclo-for.py
+++ Clase - 7/ciclo-for.py
@@ -6,3 +6,21 @@ for letra in cadena:
     print(letra)
 else:
     print("Fin ciclo for")
+
+# Sentencia break
+
+for letra in cadena:
+    if letra == "M":
+        break
+    print(letra)
+else:
+    print("Fin ciclo for")
+
+# Sentencia continue
+
+for letra in cadena:
+    if letra == "M":
+        continue
+    print(letra)
+else:
+    print("Fin ciclo for")
