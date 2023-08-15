51cd5511fa55d26ccbad7d8b634eca4f9d812137
Luis Mera <luismera86@gmail.com>
2023-08-15 20:29:44 -0300

    Clase 8 python

---
+++ clase - 8/Ejercicios/ejercicios.py
@@ -0,0 +1,16 @@
+
+# Ejercicio 1
+for i in range(10):
+    if i % 3 == 0:
+        print(i)
+        
+
+# Ejercicio 2
+
+for i in range(2,7):
+    print(i)
+    
+# Ejercicio 3
+for i in range(0, 11, 3):
+    print(i)
+
